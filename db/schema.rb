bf9b7a537caf3bb4abc9ecacf032e76b91df7f5e
Fred.schoeneman@gmail.com <fred.schoeneman@gmail.com>
2021-10-25 18:42:03 -0700

    handles mime magic

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_20_132043) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "endangereds", force: :cascade do |t|
     t.string "name"
     t.string "iucn"
